c8107bf6469bd2f1d58fcfab89d5ecf7068dbd2c
Will Welker <wwelker@gmail.com>
2018-11-30 04:28:45 -0800

    Started bash script for ffmpeg gif conversion

--- bash/make-gif.sh
+++ bash/make-gif.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+#This bash script runs a program called ffmpeg that can change audio and video formats
+#ffmpeg is probably already installed on your Raspberry Pi
+#We will use it to convert a image sequence to a video then video to animated gif
 
-./ffmpeg.exe -y -i ./seq/*.mkv \
+#make color pallete for gif
+ffmpeg -y -i ./seq/*.mkv \
 -vf fps=10,scale=200:-1:flags=lanczos,palettegen palette.png
 
-./ffmpeg.exe -i ./seq/*.mkv -i palette.png -filter_complex \
-"fps=10,scale=200:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif
+#make gif from video file usong palette.png
+ffmpeg -i ./seq/*.mkv -i palette.png -filter_complex \
+"fps=12,scale=200:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif
+
+#remove pallet.png
+rm pallete.png
+
+#rename gif to unique name so browsers won't show cached version
+#note: $1 is a built-in variable that references the first perameter used when this script was invoked
+#in this case, the node red exec node passed the msg.payload value into it
+mv output.png $1.png
