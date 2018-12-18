f6d5980272c4300197883c52de93ea73ff84fba1
Will Welker <wwelker@gmail.com>
2018-12-18 04:01:59 -0800

    Update get-scripts.sh

--- bash/get-scripts.sh
+++ bash/get-scripts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Download make-gif-sh and count-image.sh into the .node-red directory
+# Change the path below if you are using a different location
 
 cd /home/pi/.node-red
-wget 
+wget https://raw.githubusercontent.com/WillWelker/pi-temporal-camera/master/bash/make-gif.sh
+wget https://raw.githubusercontent.com/WillWelker/pi-temporal-camera/master/bash/count-gif.sh
